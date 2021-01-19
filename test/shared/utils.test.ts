8ea9eed7b1368776b37d44302222e323a055039c
mshanemc <shane.mclaughlin@salesforce.com>
2021-01-19 09:01:21 -0600

    test: coverage for alias reverse lookup

---
+++ test/shared/utils.test.ts
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020, salesforce.com, inc.
+ * All rights reserved.
+ * Licensed under the BSD 3-Clause license.
+ * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
+ */
+import { $$, expect } from '@salesforce/command/lib/test';
+import { Aliases } from '@salesforce/core';
+import { stubMethod } from '@salesforce/ts-sinon';
+import { getAliasByUsername } from '../../src/shared/utils';
+
+describe('getAliasByUsername', () => {
+  beforeEach(async () => {
+    stubMethod($$.SANDBOX, Aliases, 'create').resolves(Aliases.prototype);
+    stubMethod($$.SANDBOX, Aliases, 'getDefaultOptions').returns({});
+    stubMethod($$.SANDBOX, Aliases.prototype, 'getContents').returns({
+      orgs: {
+        alias1: 'username1',
+        alias2: 'username2',
+      },
+    });
+  });
+  afterEach(() => {
+    $$.SANDBOX.restore();
+  });
+
+  it('returns alias for a username that exists', async () => {
+    expect(await getAliasByUsername('username1')).to.equal('alias1');
+    expect(await getAliasByUsername('username2')).to.equal('alias2');
+  });
+  it('returns undefined when no matching username is found', async () => {
+    expect(await getAliasByUsername('username3')).to.be.undefined;
+  });
+});
