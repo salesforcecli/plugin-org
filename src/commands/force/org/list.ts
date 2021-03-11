3012cc04a670e4bf71e75a02e2f0981a71eb4e0d
Juliet Shackell <63259011+jshackell-sfdc@users.noreply.github.com>
2021-03-11 15:13:45 -0800

    Update list.ts

--- src/commands/force/org/list.ts
+++ src/commands/force/org/list.ts
@@ -4,6 +4,8 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
+import { EOL } from 'os';
+
 import { flags, FlagsConfig, SfdxCommand } from '@salesforce/command';
 import { AuthInfo, ConfigAggregator, ConfigInfo, Connection, Org, SfdxError, Messages } from '@salesforce/core';
 import { sortBy } from '@salesforce/kit';
