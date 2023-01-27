ca709ba682c5b18a6379d72ae4d494b7c2e415e4
mshanemc <shane.mclaughlin@salesforce.com>
2023-01-27 13:38:05 -0600

    test: remove deprecation (pending oclif bug)

--- src/commands/force/org/create.ts
+++ src/commands/force/org/create.ts
@@ -53,10 +53,11 @@ export class Create extends SfCommand<CreateResult> {
   public static readonly description = messages.getMessage('description');
   public static readonly examples = messages.getMessages('examples');
 
-  public static state = 'deprecated';
-  public static deprecationOptions = {
-    message: messages.getMessage('deprecation'),
-  };
+  // waiting for oclif bug fix around missing config.id.
+  // public static state = 'deprecated';
+  // public static deprecationOptions = {
+  //   message: messages.getMessage('deprecation'),
+  // };
 
   // needed to allow varargs
   public static readonly strict = false;
