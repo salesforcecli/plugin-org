e3091c3de3d35a2012c326b8e2d401e39c6372a8
mshanemc <shane.mclaughlin@salesforce.com>
2023-02-15 17:25:22 -0600

    test: avoid deprecation warning about fs.rmdir

--- test/nut/scratchDelete.nut.ts
+++ test/nut/scratchDelete.nut.ts
@@ -51,7 +51,7 @@ describe('env:delete:scratch NUTs', () => {
     try {
       await session?.clean();
     } catch {
-      await fs.promises.rmdir(session.dir, { recursive: true }).catch(() => {});
+      await fs.promises.rm(session.dir, { recursive: true }).catch(() => {});
     }
   });
 
