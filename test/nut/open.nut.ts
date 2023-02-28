027ef64a5a390792550cc1fb0fd4859f764f668b
Willie Ruemmele <willieruemmele@gmail.com>
2023-02-28 13:19:56 -0700

    test: try alias in org create

--- test/nut/open.nut.ts
+++ test/nut/open.nut.ts
@@ -29,6 +29,7 @@ describe('test org:open command', () => {
       scratchOrgs: [
         {
           executable: 'sfdx',
+          alias: 'default',
           duration: 1,
           setDefault: true,
           wait: 10,
@@ -100,57 +101,3 @@ describe('test org:open command', () => {
     await session.clean();
   });
 });
-//
-// import * as querystring from 'querystring';
-// import * as path from 'path';
-// import * as fs from 'fs';
-// import { expect } from '@salesforce/command/lib/test';
-// import { TestSession } from '@salesforce/cli-plugins-testkit';
-// import { execCmd } from '@salesforce/cli-plugins-testkit';
-// import { Dictionary } from '@salesforce/ts-types';
-// import { OrgOpenOutput } from '../../src/commands/org/open';
-//
-// const flexiPagePath = 'force-app/main/default/flexipages/Property_Explorer.flexipage-meta.xml';
-// const layoutDir = 'force-app/main/default/layouts';
-// const layoutFilePath = path.join(layoutDir, 'MyLayout.layout-meta.xml');
-//
-// describe('force:source:open', () => {
-//   let session: TestSession;
-//   let defaultUsername: string;
-//   let defaultUserOrgId: string;
-//
-//   before(async () => {
-//     session = await TestSession.create({
-//       project: {
-//         gitClone: 'https://github.com/trailheadapps/dreamhouse-lwc.git',
-//       },
-//       devhubAuthStrategy: 'AUTO',
-//       scratchOrgs: [
-//         {
-//           executable: 'sfdx',
-//           duration: 1,
-//           setDefault: true,
-//           wait: 10,
-//           config: path.join('config', 'project-scratch-def.json'),
-//         },
-//       ],
-//     });
-//
-//     execCmd('force:source:deploy -p force-app', { cli: 'sfdx' });
-//
-//     const defaultOrg = session.orgs.get('default');
-//     defaultUsername = defaultOrg.username;
-//     defaultUserOrgId = defaultOrg.orgId;
-//
-//     await fs.promises.writeFile(
-//       path.join(session.project.dir, layoutFilePath),
-//       '<layout xmlns="http://soap.sforce.com/2006/04/metadata">\n</layout>'
-//     );
-//   });
-//
-//   after(async () => {
-//     await session?.clean();
-//   });
-//
-//   describe('source open', () => {});
-// });
