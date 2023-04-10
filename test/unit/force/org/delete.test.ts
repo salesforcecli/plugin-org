cb328b89fc66ddbab301759ec11185464cd87981
mshanemc <shane.mclaughlin@salesforce.com>
2023-04-10 16:52:12 -0500

    test: correct mocks on org:delete

--- test/unit/force/org/delete.test.ts
+++ test/unit/force/org/delete.test.ts
@@ -7,11 +7,13 @@
 import { ConfigAggregator, Messages, Org, SfError } from '@salesforce/core';
 import { MockTestOrgData, TestContext } from '@salesforce/core/lib/testSetup';
 
-import { expect } from 'chai';
+import { expect, config } from 'chai';
 import { stubPrompter, stubSfCommandUx } from '@salesforce/sf-plugins-core';
 import { SandboxAccessor } from '@salesforce/core/lib/stateAggregator/accessors/sandboxAccessor';
 import { Config } from '@oclif/core';
 import { Delete } from '../../../../src/commands/force/org/delete';
+
+config.truncateThreshold = 0;
 
 Messages.importMessagesDirectory(__dirname);
 const messages = Messages.loadMessages('@salesforce/plugin-org', 'delete');
@@ -60,7 +62,7 @@ describe('org:delete', () => {
   });
 
   it('will determine sandbox vs scratch org and delete sandbox', async () => {
-    $$.SANDBOX.stub(SandboxAccessor.prototype, 'has').resolves(true);
+    $$.SANDBOX.stub(SandboxAccessor.prototype, 'hasFile').resolves(true);
     const res = await Delete.run(['--target-org', testOrg.username]);
     expect(prompterStubs.confirm.calledOnce).to.equal(true);
     expect(prompterStubs.confirm.firstCall.args[0]).to.equal(
@@ -82,7 +84,7 @@ describe('org:delete', () => {
   });
 
   it('will NOT prompt before deleting sandbox when flag is provided', async () => {
-    $$.SANDBOX.stub(SandboxAccessor.prototype, 'has').resolves(true);
+    $$.SANDBOX.stub(SandboxAccessor.prototype, 'hasFile').resolves(true);
     $$.SANDBOX.stub(Org.prototype, 'delete').resolves();
     const res = await Delete.run(['--noprompt', '--target-org', testOrg.username]);
     expect(prompterStubs.confirm.calledOnce).to.equal(false);
@@ -105,13 +107,14 @@ describe('org:delete', () => {
   });
 
   it('will catch the SandboxNotFound and wrap correctly', async () => {
-    $$.SANDBOX.stub(SandboxAccessor.prototype, 'has').resolves(true);
+    $$.SANDBOX.stub(SandboxAccessor.prototype, 'hasFile').resolves(true);
     $$.SANDBOX.stub(Org.prototype, 'delete').throws(new SfError('bah!', 'SandboxNotFound'));
     const res = await Delete.run(['--noprompt', '--target-org', testOrg.username]);
     expect(prompterStubs.confirm.called).to.equal(false);
     expect(res).to.deep.equal({ orgId: testOrg.orgId, username: testOrg.username });
-    expect(sfCommandUxStubs.log.getCalls().flatMap((call) => call.args)).to.deep.include(
-      messages.getMessage('sandboxConfigOnlySuccess', [testOrg.username])
-    );
+    expect(
+      sfCommandUxStubs.log.getCalls().flatMap((call) => call.args),
+      JSON.stringify(sfCommandUxStubs.log.getCalls().flatMap((call) => call.args))
+    ).to.deep.include(messages.getMessage('sandboxConfigOnlySuccess', [testOrg.username]));
   });
 });
