47d7a261826ae45ab9ec9725fb0d440d86c5a1f5
mshanemc <shane.mclaughlin@salesforce.com>
2024-01-10 08:14:53 -0600

    test: stub returns an eventEmitter

--- test/unit/org/open.test.ts
+++ test/unit/org/open.test.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-
+import { EventEmitter } from 'node:events';
 import fs from 'node:fs';
 import { join } from 'node:path';
 import { assert, expect } from 'chai';
@@ -44,7 +44,7 @@ describe('org:open', () => {
     stubUx($$.SANDBOX);
     stubSpinner($$.SANDBOX);
     await $$.stubAuths(testOrg);
-    spies.set('open', stubMethod($$.SANDBOX, utils, 'openUrl').resolves());
+    spies.set('open', stubMethod($$.SANDBOX, utils, 'openUrl').resolves(new EventEmitter()));
   });
 
   afterEach(() => {
