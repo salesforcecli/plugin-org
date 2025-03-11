7f58889e11f09fe4d35e5a8a5919d18dc62cdd9e
soridalac <sorida.lac@salesforce.com>
2025-03-11 13:49:59 -0700

    fix: add the exclusive flag to edition and source-org

--- src/commands/org/create/scratch.ts
+++ src/commands/org/create/scratch.ts
@@ -80,6 +80,7 @@ export default class OrgCreateScratch extends SfCommand<ScratchCreateResponse> {
         'partner-group',
         'partner-professional',
       ],
+      exclusive: ['snapshot', 'source-org'],
       // eslint-disable-next-line @typescript-eslint/require-await
       parse: async (value: string) => {
         // the API expects partner editions in `partner <EDITION>` format.
@@ -159,6 +160,7 @@ export default class OrgCreateScratch extends SfCommand<ScratchCreateResponse> {
     'source-org': Flags.salesforceId({
       summary: messages.getMessage('flags.source-org.summary'),
       description: messages.getMessage('flags.source-org.description'),
+      exclusive: ['edition', 'snapshot'],
       startsWith: '00D',
       length: 15,
       helpGroup: definitionFileHelpGroupName,
